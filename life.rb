32c7f898ccc56b030eae1f232857a3cf2e845eb0
lpil <louis@lpil.co.uk>
2014-03-05 13:43:35 +0000

    Done. Game::play

--- life.rb
+++ life.rb
@@ -1,8 +1,18 @@
 #!/usr/bin/env ruby
 
+# Our game
 class Game
   def initialize(height, width)
     @board = Array.new(height) { Array.new(width) { Cell.new } }
+  end
+
+  def play
+    loop do
+      (system 'clear')
+      puts self
+      sleep 0.08
+      next_gen
+    end
   end
 
   def to_s
@@ -10,13 +20,41 @@ class Game
       row.map { |cell| cell.to_s }.join
     end.join "\n"
   end
+
+  private
+
+  def next_gen
+    @board.each_with_index do |row, y|
+      row.each_with_index do |cell, x|
+        cell.neighbours = count_neighbours y, x
+      end
+    end
+    @board.flatten.each { |cell| cell.next }
+  end
+
+  def count_neighbours(y, x)
+    [
+      [-1, -1], [-1,+0], [-1,+1],
+      [+0, -1],          [+0,+1],
+      [+1, -1], [+1,+0], [+1,+1]
+    ].reduce(0) do |a, e|
+      ny = (y + e[0]) % @board.size
+      nx = (x + e[1]) % @board[0].size
+      a + @board[ny][nx].to_i
+    end
+  end
 end
 
+# Cell
 class Cell
-  attr_writer :neighbours
+  attr_writer :neighbours, :alive
 
-  def next!
-    @alive = @alive ? (2..3) === @neighbours : 3 == @neighbours
+  def initialize
+    @alive = true if rand > 0.9
+  end
+
+  def next
+    @alive = @alive ? (2..3).include?(@neighbours) : 3 == @neighbours
   end
 
   def to_i
@@ -24,6 +62,12 @@ class Cell
   end
 
   def to_s
-    @alive ? '#' : ' '
+    @alive ? 'o' : ' '
   end
+end
+
+begin
+  Game.new(24, `tput cols`.to_i).play
+rescue Interrupt => _
+  puts
 end
